8427b32580efafa2f0cb9d562f1efc629f3885f4
gopiyadav989 <gopiyadu17@gmail.com>
2025-04-16 21:38:02 +0530

    push

--- backend/index.js
+++ backend/index.js
@@ -34,9 +34,10 @@ app.get('/api/health', (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, '/frontend/dist')));
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-});
+  });
+  
 
 
 
